eaab50e54b0d07b89bf11bc053a387a4d99f1821
NamTran <nam.tran@zamo.io>
2021-10-18 17:47:09 +0700

    a

--- src/main/java/com/TestApplication.java
+++ src/main/java/com/TestApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EnglishCenterMain {
+public class TestApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(EnglishCenterMain.class, args);
+        SpringApplication.run(TestApplication.class, args);
     }
+
 }
