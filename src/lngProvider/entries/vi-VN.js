2588d89fc43d0e300ca56fafa384b81d44d6504d
NamTran <nam.tran@zamo.io>
2021-10-28 23:18:34 +0700

    b

---
+++ src/lngProvider/entries/vi-VN.js
@@ -0,0 +1,12 @@
+import appLocaleData from "react-intl/locale-data/en";
+import viMessages from "../locales/vi_VN.json";
+
+const ViLang = {
+  messages: {
+    ...viMessages
+  },
+  antd: null,
+  locale: 'en-US',
+  data: appLocaleData
+};
+export default ViLang;
