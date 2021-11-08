fd6c185403c4f9c1f0ad565256eee5bec405befd
NamTran <nam.tran@zamo.io>
2021-11-08 20:29:35 +0700

    xóa những thử k dung trong appRedux

--- src/appRedux/reducers/index.js
+++ src/appRedux/reducers/index.js
@@ -2,8 +2,6 @@ import {combineReducers} from 'redux'
 import {connectRouter} from 'connected-react-router'
 import Settings from "./Settings";
 import Auth from "./Auth";
-import Notes from "./Notes";
-import Contact from "./Contact";
 import Common from "./Common";
 
 
@@ -11,8 +9,6 @@ const createRootReducer = (history) => combineReducers({
     router: connectRouter(history),
     settings: Settings,
     auth: Auth,
-    notes: Notes,
-    contact: Contact,
     common: Common,
 });
 
