dc8406a3015c3271d28b4a0033a94abe048e5462
NamTran <nam.tran@zamo.io>
2021-11-09 09:36:56 +0700

    chỉnh file config firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,30 +1,17 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/storage";
 
-// Initialize Firebase
 const config = {
-    apiKey: "AIzaSyAz-GPfA-hN74oFh3XvXsF9vQrlE5xpU10",
-    authDomain: "wieldy-4f59c.firebaseapp.com",
-    databaseURL: "https://wieldy-4f59c.firebaseio.com",
-    projectId: "wieldy-4f59c",
-    storageBucket: "wieldy-4f59c.appspot.com",
-    messagingSenderId: "81949884261"
+    apiKey: "AIzaSyCwPEAnFH2e1o_-VfUWi7KYl4Ax9NfWAz4",
+    authDomain: "englishcenter-bd4ab.firebaseapp.com",
+    projectId: "englishcenter-bd4ab",
+    storageBucket: "englishcenter-bd4ab.appspot.com",
+    messagingSenderId: "786660928325",
+    appId: "1:786660928325:web:d9804ec02de059e894ec2c",
+    measurementId: "G-JE1GX84JGQ"
 };
-
 firebase.initializeApp(config);
-const auth = firebase.auth();
 
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
-const githubAuthProvider = new firebase.auth.GithubAuthProvider();
-const twitterAuthProvider = new firebase.auth.TwitterAuthProvider();
+const storage = firebase.storage();
 
-const database = firebase.database();
-
-export {
-    database,
-    auth,
-    googleAuthProvider,
-    githubAuthProvider,
-    facebookAuthProvider,
-    twitterAuthProvider
-};
+export {storage, firebase as default};
