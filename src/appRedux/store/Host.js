77d1b03027a87992ce598e9d6eca7806e9ca66e3
Nguyen Tran Phuc <nguyentranphuc.0310@gmail.com>
2021-11-10 13:43:31 +0700

    tiếng việt cho datepicker, chỉnh lại font cho template

--- src/appRedux/store/Host.js
+++ src/appRedux/store/Host.js
@@ -1,2 +1,2 @@
-export const host = "http://localhost:8080";
-// export const host = "https://englishcenter-spkt-api.herokuapp.com";
+//export const host = "http://localhost:8080";
+export const host = "https://englishcenter-spkt-api.herokuapp.com";
--- src/routes/admin/userPage/studentPage/index.js
+++ src/routes/admin/userPage/studentPage/index.js
@@ -1,12 +1,17 @@
 import React, {useEffect, useState} from "react";
-import {Button, Card, Col, DatePicker, Form, Input, Modal, Row, Select, Table} from "antd";
+import {Button, Card, Col, ConfigProvider, DatePicker, Form, Input, Modal, Row, Select, Table} from "antd";
 import IntlMessages from "../../../../util/IntlMessages";
 import {useDispatch, useSelector} from "react-redux";
 import {addMember, getListMember} from "../../../../appRedux/actions";
 import {getDate, getGender} from "../../../../util/ParseUtils";
 import {PlusOutlined, SearchOutlined} from "@ant-design/icons";
 import Image from "../../../../components/uploadImage";
-import {createNotification} from "../../../../components/Notification";
+import moment from 'moment'
+
+moment.updateLocale('vi', {
+    weekdaysMin : ["Cn", "T2", "T3", "T4", "T5", "T6", "T7"],
+    monthsShort: ["Tháng 1","Tháng 2","Tháng 3","Tháng 4","Tháng 5","Tháng 6","Tháng 7","Tháng 8","Tháng 9","Tháng 10","Tháng 11","Tháng 12"]
+    });
 
 const {RangePicker} = DatePicker;
 let param = {
