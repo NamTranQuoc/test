382a566af6598e295b3d0f6fb7c7e93a1395e787
NamTran <nam.tran@zamo.io>
2021-11-10 14:16:42 +0700

    thêm một số server api

--- src/appRedux/store/Host.js
+++ src/appRedux/store/Host.js
@@ -1,2 +1,6 @@
 //export const host = "http://localhost:8080";
-export const host = "https://englishcenter-spkt-api.herokuapp.com";
+// export const host = "https://englishcenter-spkt-api.herokuapp.com";
+export const host = "https://englishcenter-spkt-api-v2.herokuapp.com";
+// export const host = "https://englishcenter-spkt-api-v3.herokuapp.com";
+// export const host = "https://englishcenter-spkt-api-v4.herokuapp.com";
+// export const host = "https://englishcenter-spkt-api-v5.herokuapp.com";
